9c1704b1445041a0abcca1b654405e2efd76b1e5
Jackson Chakma <jacksonchakma260@gmail.com>
2025-11-22 22:46:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+ const express = require('express')
+ const cors =require ('cors')
+ require ('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+
+// middlewear 
+app.use(express.json())
+app.use(cors.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
